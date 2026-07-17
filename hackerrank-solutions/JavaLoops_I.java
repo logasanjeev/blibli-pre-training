0d4f70b5bac245254c6618799c464e4708ffa9bf
logasanjeev <133761180+logasanjeev@users.noreply.github.com>
2026-07-17 22:59:40 +0530

    Add Java solutions for various HackerRank challenges including If-Else, Loops, Output Formatting, and Stdin/Stdout

---
+++ hackerrank-solutions/JavaLoops_I.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class JavaLoops_I {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + N * i);
+        }
+    }
+}
